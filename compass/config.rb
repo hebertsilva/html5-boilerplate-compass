25f1d5a59843227cd3e6fda661587d015a12491a
Hebert Silva <hebertpereirasilva@gmail.com>
2014-06-17 15:21:23 -0300

    first commit, init project HTML Boilerplate using compass

---
+++ compass/config.rb
@@ -0,0 +1,17 @@
+static_dir = "../static/"
+
+css_dir = static_dir +  "css/"
+sass_dir = "source"
+images_dir = static_dir + "image/"
+javascripts_dir = static_dir + "js/"
+fonts_dir = static_dir + "font/"
+generated_images_dir = images_dir + "common/"
+
+output_style = :compressed
+relative_assets = true
+line_comments = false
+sass_options = { :debug_info => false }
+
+http_path = "http://127.0.0.1/"
+http_images_path = "/html5-boilerplate-compass/images/"
+http_generated_images_path = "/html5-boilerplate-compass/images/"   
