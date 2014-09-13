600b1288c1d176b8f90e5aa0fc6a8d2b1add08c0
Hebert Silva <hebertsilva@MacBook-Pro-de-Hebert.local>
2014-09-12 23:25:33 -0300

    init JS Base

---
+++ static/js/views/BaseView.js
@@ -0,0 +1,19 @@
+App = window.App || {};
+
+App.objects = {};
+
+App.objects.BaseView = function() {
+	this.win = $(window);
+
+	this.initEvents();
+}
+
+App.objects.BaseView.prototype = {
+	initEvents: function() {
+		// Init Events
+	}
+}
+
+$(function() {
+	App.objects.baseView = new App.objects.BaseView();
+});
